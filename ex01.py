e34cb0461e7212cc07eb361389b6eac655360507
Pedro Hammes <81041208+PedroHammes@users.noreply.github.com>
2026-09-04 22:31:46 +0000

    exercicio 1

---
+++ ex01.py
@@ -0,0 +1,12 @@
+
+def media():
+    n1 = float(input("Digite  a nota 1: "))
+    n2 = float(input("Digite a nota 2: "))
+
+    media = (n1+n2)/2
+    print("A media é: ", media)
+
+print("Aluno 1")
+media()
+print("Aluno 2")
+media()
