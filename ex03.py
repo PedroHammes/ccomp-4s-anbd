158166f08b66acbea9def8451a372bfb57ffe603
Pedro Hammes <81041208+PedroHammes@users.noreply.github.com>
2026-09-04 23:13:46 +0000

    refact: otimizar alg 03

--- ex03.py
+++ ex03.py
@@ -1,4 +1,10 @@
-def validate (d, m, y):
+def validate ():
+
+    d = int(input("Informe o dia: "))
+    m = int(input("Informe o mês: "))
+    y = int(input("Informe o ano: "))
+
+
     if d > 31 or d < 1:
         print ("dia inválido")
     elif m > 12 or m < 1:
@@ -6,11 +12,13 @@ def validate (d, m, y):
     elif y > 2026:
         return print("Ano inválido")
 
+    return verify(y)
 
+def verify (y):
     if (y % 4) == 0:
         return print("Bissexto")
     elif (y % 4) != 0:
         return print("Não bissexto")
 
 
-validate(20, 12, 2022)
+validate() 
